654ec7fe130bd1a768aa0f141ea643da15ba5af3
Frerich Raabe <raabe@froglogic.com>
2018-12-02 13:34:13 +0100

    Solution for day 2 in Haskell

---
+++ haskell/day2/Main.hs
@@ -0,0 +1,34 @@
+import Data.List (group, sort, minimumBy, tails)
+import Data.Ord (comparing)
+
+
+main :: IO ()
+main = do
+    ids <- lines <$> readFile "input.txt"
+    putStrLn ("Part one: " ++ show (partOne ids))
+    putStrLn ("Part two: " ++ show (partTwo ids))
+
+
+partOne :: [String] -> Int
+partOne ids = length idsWithDoublettes * length idsWithTriplets
+  where
+    idsWithDoublettes = filter (any ((== 2) . snd)) histograms
+    idsWithTriplets = filter (any ((== 3) . snd)) histograms
+    histograms = map histogram ids
+
+
+partTwo :: [String] -> String
+partTwo ids = [a | (a,b) <- zip correctIdA correctIdB, a == b]
+  where
+    (correctIdA, correctIdB) = minimumBy (comparing numDifferences) (pairs ids)
+    numDifferences (as,bs) = length [a | (a,b) <- zip as bs, a /= b]
+
+
+-- | Counts how often each element in the input list occurs
+histogram :: Ord a => [a] -> [(a, Int)]
+histogram = map (\g -> (head g, length g)) . group . sort
+
+
+-- | Yields all ways to pick two elements from a list (ignoring ordering)
+pairs :: [a] -> [(a,a)]
+pairs xs = [(a,b) | (a:as) <- tails xs, b <- as]
