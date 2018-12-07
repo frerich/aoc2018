548300095b56a9b73911f571a5412a1647f880b5
Frerich Raabe <raabe@froglogic.com>
2018-12-07 14:23:32 +0100

    Day 7 part 2 in Rust
    This is a really terribly solution, I had to fight quite a bit with the
    language to make this work. :-/

--- rust/day7/src/main.rs
+++ rust/day7/src/main.rs
@@ -20,7 +20,8 @@ fn main() {
 
     let deps = parse(&lines);
 
-    println!("Part one: {:?}", String::from_iter(part_one(&deps)));
+    println!("Part one: {}", String::from_iter(part_one(&deps)));
+    println!("Part two: {}", part_two(&deps));
 }
 
 fn parse(lines: &Vec<String>) -> Dependencies {
@@ -39,7 +40,7 @@ fn parse(lines: &Vec<String>) -> Dependencies {
 fn find_first_steps(deps: &Dependencies) -> Vec<Step> {
     // TODO: chain iterators
     let mut all_next_steps: HashSet<Step> = HashSet::new();
-    for (step_name, next_step_names) in deps {
+    for next_step_names in deps.values() {
         for next_step_name in next_step_names {
             all_next_steps.insert(*next_step_name);
         }
@@ -82,3 +83,46 @@ fn part_one(deps: &Dependencies) -> Vec<Step> {
     result
 }
 
+fn part_two(deps: &Dependencies) -> u32 {
+    let mut result = 0;
+    let mut workers: Vec<(Step,u8)> = Vec::new();
+    let mut finished_steps: Vec<Step> = Vec::new();
+    let mut step_queue: Vec<Step> = find_first_steps(&deps);
+
+    while !step_queue.is_empty() || !workers.is_empty() {
+        println!("tick {}: step_queue = {:?}, workers: {:?}", result, step_queue, workers);
+        let (mut busy_workers, finished_workers) : (Vec<(Step,u8)>, Vec<(Step,u8)>) = workers.iter().partition(|(_step, time_left)| *time_left > 1);
+        for (_step, time_left) in & mut busy_workers {
+            *time_left -= 1;
+        }
+
+        for (step, _time_left) in &finished_workers {
+            finished_steps.push(*step);
+            if let Some(step_deps) = deps.get(&step) {
+                step_queue.extend(step_deps.iter().filter(|s| !finished_steps.contains(s)));
+                step_queue.sort();
+                step_queue.dedup();
+            }
+        }
+
+        workers = busy_workers;
+
+        while workers.len() < 5 && !step_queue.is_empty() {
+            // println!("have only {} workers, want to dispatch next item from step_queue {:?}, finished_steps: {:?}", workers.len(), step_queue, finished_steps);
+            let next_ready_step = step_queue.iter().filter(|step| is_ready(**step, &finished_steps, deps)).map(|step| *step).min();
+            if let Some(step) = next_ready_step {
+                // println!("dispatching step {}", step);
+                step_queue.retain(|s| s != &step);
+                let duration = step as u8 - 'A' as u8 + 61;
+                workers.push((step, duration));
+            } else {
+                // println!("no further step is ready!");
+                break;
+            }
+        }
+
+        result = result + 1;
+    }
+
+    result - 1
+}
