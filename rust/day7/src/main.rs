7e9156dce8a622b2763065e2709ce08aa34ef2b7
Frerich Raabe <raabe@froglogic.com>
2018-12-08 00:24:12 +0100

    Major overhaul for today's solution in Rust

--- rust/day7/src/main.rs
+++ rust/day7/src/main.rs
@@ -7,6 +7,8 @@ use std::iter::FromIterator;
 
 
 type Step = char;
+
+// The Depndencies map associates each step with the set of next steps.
 type Dependencies = HashMap<Step, Vec<Step>>;
 
 
@@ -24,57 +26,64 @@ fn main() {
     println!("Part two: {}", part_two(&deps));
 }
 
+
+// Parse the puzzle data into a Dependency table
 fn parse(lines: &Vec<String>) -> Dependencies {
     let mut deps = Dependencies::new();
     for line in lines {
         let step_name = line.chars().nth(5).unwrap();
         let next_step_name = line.chars().nth(36).unwrap();
 
-        // TODO: Insert with default in one go.
-        deps.entry(step_name).or_default();
-        deps.get_mut(&step_name).unwrap().push(next_step_name);
+        deps
+            .entry(step_name)
+            .or_insert(Vec::new())
+            .push(next_step_name);
     }
     deps
 }
 
-fn find_first_steps(deps: &Dependencies) -> Vec<Step> {
-    // TODO: chain iterators
-    let mut all_next_steps: HashSet<Step> = HashSet::new();
-    for next_step_names in deps.values() {
-        for next_step_name in next_step_names {
-            all_next_steps.insert(*next_step_name);
-        }
-    }
 
-    // TODO: Maybe we can return an iterator directly?
-    deps.keys().filter(|step| !all_next_steps.contains(step)).map(|step| *step).collect()
-}
+// Given a dependency table, yield the initial steps (i.e. those which are not
+// successors of any steps).
+fn initial_steps(deps: &Dependencies) -> Vec<Step> {
+    let all_next_steps: HashSet<&Step> = deps.values().flatten().collect();
 
-fn find_dependencies(step: Step, deps: &Dependencies) -> Vec<Step>
-{
     deps
-        .iter()
-        .filter_map(|(step_name, next)| if next.contains(&step) { Some(*step_name) } else { None })
+        .keys()
+        .filter_map(|step| if all_next_steps.contains(step) { None } else { Some(*step) })
         .collect()
 }
 
-fn is_ready(step: Step, completed_steps: &Vec<Step>, deps: &Dependencies) -> bool {
-    find_dependencies(step, deps).iter().all(|step| completed_steps.contains(step))
+
+// Yield the steps which have to precede the given step, i.e. the prerequisites
+fn preceding_steps(step: &Step, deps: &Dependencies) -> Vec<Step>
+{
+    deps
+        .iter()
+        .filter_map(|(step_name, next)| if next.contains(step) { Some(*step_name) } else { None })
+        .collect()
 }
+
+
+// Indicates whether the given step is ready: it is 'readyy' when all preceding steps are complete.
+fn is_ready(step: &Step, completed_steps: &Vec<Step>, deps: &Dependencies) -> bool {
+    preceding_steps(step, deps).iter().all(|step| completed_steps.contains(step))
+}
+
 
 fn part_one(deps: &Dependencies) -> Vec<Step> {
     let mut result: Vec<Step> = Vec::new();
 
-    let mut steps = find_first_steps(&deps);
+    let mut steps: HashSet<Step> = initial_steps(&deps).iter().cloned().collect();
 
-    while !steps.is_empty() {
-        // TODO It would be better to not check result.contains all the time!
-        let next_ready_step = steps.iter().filter(|step| !result.contains(step) && is_ready(**step, &result, deps)).map(|step| *step).min();
+    loop {
+        let next_ready_step = steps.iter().filter(|step| is_ready(step, &result, deps)).cloned().min();
         match next_ready_step {
             Some(step) => {
+                steps.remove(&step);
                 result.push(step);
-                if let Some(step_deps) = deps.get(&step) {
-                    steps.extend(step_deps);
+                if let Some(next_steps) = deps.get(&step) {
+                    steps.extend(next_steps);
                 }
             }
             None => break
@@ -83,46 +92,50 @@ fn part_one(deps: &Dependencies) -> Vec<Step> {
     result
 }
 
+
 fn part_two(deps: &Dependencies) -> u32 {
-    let mut result = 0;
+    const NUM_WORKERS: usize = 5;
+
+    let mut num_ticks = 0;
     let mut workers: Vec<(Step,u8)> = Vec::new();
     let mut finished_steps: Vec<Step> = Vec::new();
-    let mut step_queue: Vec<Step> = find_first_steps(&deps);
+    let mut step_queue: HashSet<Step> = initial_steps(&deps).iter().cloned().collect();
 
     while !step_queue.is_empty() || !workers.is_empty() {
-        println!("tick {}: step_queue = {:?}, workers: {:?}", result, step_queue, workers);
-        let (mut busy_workers, finished_workers) : (Vec<(Step,u8)>, Vec<(Step,u8)>) = workers.iter().partition(|(_step, time_left)| *time_left > 1);
-        for (_step, time_left) in & mut busy_workers {
-            *time_left -= 1;
+        // Tick down all workers
+        for (_step, time_left) in & mut workers {
+            *time_left -= 1
         }
 
-        for (step, _time_left) in &finished_workers {
-            finished_steps.push(*step);
-            if let Some(step_deps) = deps.get(&step) {
-                step_queue.extend(step_deps.iter().filter(|s| !finished_steps.contains(s)));
-                step_queue.sort();
-                step_queue.dedup();
-            }
-        }
+        // Identify busy workers (which require more time) and finished ones
+        let (busy_workers, finished_workers) : (Vec<(Step,u8)>,Vec<(Step,u8)>)
+            = workers.iter().partition(|(_step, time_left)| *time_left > 0);
 
+        // We'll continiue workiing with the busy workers
         workers = busy_workers;
 
-        while workers.len() < 5 && !step_queue.is_empty() {
-            // println!("have only {} workers, want to dispatch next item from step_queue {:?}, finished_steps: {:?}", workers.len(), step_queue, finished_steps);
-            let next_ready_step = step_queue.iter().filter(|step| is_ready(**step, &finished_steps, deps)).map(|step| *step).min();
-            if let Some(step) = next_ready_step {
-                // println!("dispatching step {}", step);
-                step_queue.retain(|s| s != &step);
-                let duration = step as u8 - 'A' as u8 + 61;
-                workers.push((step, duration));
-            } else {
-                // println!("no further step is ready!");
-                break;
+        // For the finished workers, check which step they processed and queue the next steps
+        for (step, _time_left) in finished_workers {
+            finished_steps.push(step);
+            if let Some(next_steps) = deps.get(&step) {
+                step_queue.extend(next_steps.iter().filter(|s| !finished_steps.contains(s)));
             }
         }
 
-        result = result + 1;
+        // Try to keep all workers busy
+        while workers.len() < NUM_WORKERS && !step_queue.is_empty() {
+            let next_ready_step = step_queue.iter().filter(|step| is_ready(step, &finished_steps, deps)).cloned().min();
+            match next_ready_step {
+                Some(step) => {
+                    step_queue.remove(&step);
+                    let duration = step as u8 - 'A' as u8 + 61;
+                    workers.push((step, duration));
+                }
+                None => break
+            }
+        }
+        num_ticks += 1
     }
 
-    result - 1
+    num_ticks - 1
 }
